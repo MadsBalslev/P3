687019674ea4f12d75dc3ce75b63aa2529d16369
Simon <simonahtx16@gmail.com>
2021-10-08 10:21:16 +0200

    same as before

---
+++ frontend/Models/Institution.cs
@@ -0,0 +1,9 @@
+public class Institution
+{
+    public Institution(string institutionNavn)
+    {
+        InstitutionNavn = institutionNavn;
+    }
+    public string InstitutionNavn { get; set; }
+
+}
