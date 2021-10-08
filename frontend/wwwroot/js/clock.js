34421eace7381938eea49db8f4b0c537bd45270a
Mads Balslev <madspbalslev@gmail.com>
2021-10-08 14:10:25 +0200

    Fix merge?

--- frontend/wwwroot/js/clock.js
+++
@@ -1,6 +0,0 @@
-function GetDateTime() {
-  const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: "2-digit", minute: "2-digit" };
-  let currentTime = new Date();
-  let date = currentTime.toLocaleDateString('da-DK', dateOptions);
-  document.querySelector(".date").innerHTML = date;
-}
