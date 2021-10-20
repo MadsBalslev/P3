e4e46d4854b6c94e88e63efc98ef83b519479c76
Mads Balslev <madspbalslev@gmail.com>
2021-10-20 11:03:15 +0200

    Add props to User-class

--- server/Models/User.cs
+++ server/Models/User.cs
@@ -2,6 +2,21 @@ namespace server.Models
 {
     public class User
     {
-        
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Email {get; set;}
+        public int PhoneNumber {get; set;}
+        public UserRole Role {get; set;}
+        public Institution Institution {get; set;}
+
+        public User(string fName, string lName, string email, int phone, UserRole role, Institution inst)
+        {
+            this.FirstName = fName;
+            this.LastName = lName;
+            this.Email = email;
+            this.PhoneNumber = phone;
+            this.Role = role;
+            this.Institution = inst;
+        }
     }
 }
