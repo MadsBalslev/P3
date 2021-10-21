dc6f323bf7bb65b554072ce24089a76a993e8b52
Casper Ståhl <cstahl20@student.aau.dk>
2021-10-21 13:07:13 +0200

    Create Poster.cs
    Used in PosterManagement

---
+++ frontend/model/Poster.cs
@@ -0,0 +1,13 @@
+class Poster
+{
+    public string Name { get; set; }
+    public string Creator { get; set; }
+    public string Institution { get; set; }
+
+    public Poster(string name, string creator, string institution)
+    {
+        Name = name;
+        Creator = creator;
+        Institution = institution;
+    }
+}
