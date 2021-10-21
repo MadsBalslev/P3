5c6539638954b0d1e5921caa9763b85a1b433c89
Mads Balslev <madspbalslev@gmail.com>
2021-10-21 13:10:42 +0200

    Do stuff

--- server/Models/User.cs
+++ server/Models/User.cs
@@ -1,7 +1,7 @@
 namespace server.Models
 {
     public class User
-    {
+    {   
         public string FirstName {get; set;}
         public string LastName {get; set;}
         public string Email {get; set;}
