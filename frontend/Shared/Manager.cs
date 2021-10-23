1cc17ff583f1a1043e6eb71af1bfa0e0966e8eb8
CasperStaahl <cstahl20@student.aau.dk>
2021-10-24 00:47:46 +0200

    revert this

--- frontend/Shared/Manager.cs
+++
@@ -1,7 +0,0 @@
-namespace frontend.Shared
-{
-    public partial class Manager<TManageable> where TManageable : IManageable
-    {
-        string _test { get; set; } = "test";
-    }
-}
--- frontend/model/Poster.cs
+++ frontend/model/Poster.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections;
 
 class Poster
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Creator { get; set; }
     public string Institution { get; set; }
+    public string imageUrl { get; set; }
+    public DateTime startDate { get; set; }
+    public DateTime endDate { get; set; }
 
     public Poster(string name, string creator, string institution)
     {
