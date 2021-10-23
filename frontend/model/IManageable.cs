395a8de118e0b6a7840d9457ea30e382aa681e3f
CasperStaahl <cstahl20@student.aau.dk>
2021-10-24 01:42:48 +0200

    start work on the generic manager
    this might be retarded

--- frontend/model/IManageable.cs
+++ frontend/model/IManageable.cs
@@ -1,7 +1,17 @@
-using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 
 public interface IManageable
 {
+    public List<string> TableHeader { get; }
+    public List<string> MudTableDataNames { get; }
 
-
+    public object this[string propertyName]
+    {
+        get
+        {
+            PropertyInfo property = GetType().GetProperty(propertyName);
+            return property.GetValue(this, null);
+        }
+    }
 }
--- frontend/model/Poster.cs
+++ frontend/model/Poster.cs
@@ -1,10 +1,23 @@
-using System.Collections;
+using System.Collections.Generic;
 
-class Poster
+class Poster : IManageable
 {
+
     public string Name { get; set; }
     public string Creator { get; set; }
     public string Institution { get; set; }
+
+    public List<string> HeaderContent
+    {
+        get
+        {
+            return new List<string>() { "Name", "Creator", "Institution" };
+        }
+    }
+
+    public List<string> TableHeader => new List<string>() {"Name", "Creator", "Institution"};
+
+    public List<string> MudTableDataNames => new List<string>() {"Name", "Creator", "Institution"};
 
     public Poster(string name, string creator, string institution)
     {
