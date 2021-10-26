f9cafe72393c298f9ed0907bd6f3c8c4cdf7071d
Frederikmoeller <70747287+Frederikmoeller@users.noreply.github.com>
2021-10-26 10:56:04 +0200

    Co-authored-by: CasperStaahl <CasperStaahl@users.noreply.github.com>

--- frontend/model/Poster.cs
+++ frontend/model/Poster.cs
@@ -4,26 +4,44 @@ using System.Collections.Generic;
 public class Poster
 {
     [AccessLevelAttribute(AccessLevel.User)]
+    [HeaderDisplaynameAttribute("Name")]
     public string Name { get; set; } = "nil";
 
     [AccessLevelAttribute(AccessLevel.InstAdmin)]
+    [HeaderDisplaynameAttribute("Creator")]
     public string Creator { get; set; } = "nil";
 
     [AccessLevelAttribute(AccessLevel.InstAdmin)]
+    [HeaderDisplaynameAttribute("Institution")]
     public string Institution { get; set; } = "nil";
 
     [AccessLevelAttribute(AccessLevel.User)]
-    public string StartDate { get; set; } = "nil";
+    [HeaderDisplaynameAttribute("Start date")]
+    public string StartDate 
+    { 
+        get { return _startDate.ToString(); } 
+        set { _startDate = DateTime.Parse(value);} 
+    }
+
+    private DateTime _startDate = new DateTime(0, 0, 0);
 
     [AccessLevelAttribute(AccessLevel.User)]
-    public string EndDate { get; set; } = "nil";
+    [HeaderDisplaynameAttribute("End date")]
+    public string EndDate 
+    { 
+        get { return _endDate.ToString(); } 
+        set { _endDate = DateTime.Parse(value); } 
+    }
+
+    private DateTime _endDate = new DateTime(0, 0, 0);
 
     [AccessLevelAttribute(AccessLevel.InstAdmin)]
+    [HeaderDisplaynameAttribute("ID")]
     public string Id { get; set; } = "nil";
 
     private string ImageUrl { get; set; } = "nil";
 
-    public Poster(string name, string creator, string institution)
+    public Poster(string name, string creator, string institution, string startDate)
     {
         Name = name;
         Creator = creator;
