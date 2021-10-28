d7d4ae25a0f108a348d4e93c1e6c51c9f74c5cf6
CasperStaahl <cstahl20@student.aau.dk>
2021-10-28 08:56:34 +0200

    Actions (#133)
    * Create dotnet-format.yml
    
    Update dotnet.yml
    
    Create dotnet-format
    
    Update dotnet-format
    
    add file tag
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    s
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    s
    
    test
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    Update dotnet-format.yml
    
    * final commit i swear
    
    * Damn
    
    * Update dotnet-format.yml
    
    * Update dotnet-format.yml
    
    * Update dotnet-format.yml
    
    * Update dotnet-format.yml
    
    * Update dotnet-format.yml
    
    * Update dotnet-format.yml
    
    * Update dotnet-format.yml
    
    * Update dotnet-format.yml
    
    * Update dotnet-format.yml
    
    * s
    
    * Update dotnet-format.yml
    
    * Update dotnet-format.yml
    
    * Automated dotnet-format update
    
    * Update dotnet-format.yml
    
    * test
    
    * s
    
    * s
    
    * Test
    
    I swear i'm done now.
    
    * Automated dotnet-format update
    
    * Add dotnet-format.yml, update dotnet.yml
    
    * test
    
    * s
    
    * s
    
    * Test
    
    I swear i'm done now.
    
    * Update dotnet-format.yml
    
    * Automated dotnet-format update
    
    Co-authored-by: Github Actions <actions@github.com>

--- server/Services/DatabaseService.cs
+++ server/Services/DatabaseService.cs
@@ -1,19 +1,19 @@
-using MySql.Data.MySqlClient;    
+using MySql.Data.MySqlClient;
 
 namespace server.Services
 {
     public class DatabaseService
     {
-        public string ConnectionString { get; set; }    
-    
-        public DatabaseService(string connectionString)    
-        {    
-            this.ConnectionString = connectionString;    
-        }    
-    
-        private MySqlConnection GetConnection()    
-        {    
-            return new MySqlConnection(ConnectionString);    
-        }  
+        public string ConnectionString { get; set; }
+
+        public DatabaseService(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
     }
 }
