8a47f0628aa97f378ba8dc92d136bebdecd2b86b
Mads Balslev <madspbalslev@gmail.com>
2021-11-01 13:48:16 +0100

    Add services

--- server/Controllers/UsersController.cs
+++ server/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using server.Models;
+using server.Services;
 
 namespace server.Controllers
 {
@@ -13,50 +14,51 @@ namespace server.Controllers
     [Route("[controller]")]
     public class UsersController : ControllerBase
     {
-        private readonly databaseContext _context;
+        // private readonly databaseContext _context;
+        private UserService _userService;
 
         public UsersController(databaseContext context)
         {
-            _context = context;
+            _userService = new UserService(context);
         }
 
         [HttpGet]
         public IEnumerable<User> Get()
         {
-            //await Task.Delay(3000);
-            return _context.Users.ToList();
+            return _userService.GetAllUsers();
+            // return _context.Users.ToList();
         }
 
-        [HttpGet("{id:int}")]
-        public ActionResult<User> GetUserDetails(int id)
-        {
-            User user = _context.Users.Where(u => u.Id == id).FirstOrDefault();
+        // [HttpGet("{id:int}")]
+        // public ActionResult<User> GetUserDetails(int id)
+        // {
+        //     User user = _context.Users.Where(u => u.Id == id).FirstOrDefault();
 
-            if (user == null)
-            {
-                return NotFound();
-            }
+        //     if (user == null)
+        //     {
+        //         return NotFound();
+        //     }
 
-            return user;
-        }
+        //     return user;
+        // }
 
-        [HttpPost]
-        public ActionResult<User> Post([FromBody] User user)
-        {
-            _context.Users.Add(user);
-            _context.SaveChanges();
+        // [HttpPost]
+        // public ActionResult<User> Post([FromBody] User user)
+        // {
+        //     _context.Users.Add(user);
+        //     _context.SaveChanges();
 
-            return _context.Users.Where(u => u.Email == user.Email).FirstOrDefault();
-        }
+        //     return _context.Users.Where(u => u.Email == user.Email).FirstOrDefault();
+        // }
 
-        [HttpDelete("{id:int}")]
-        public ActionResult<User> Delete(int id)
-        {
-            User user = _context.Users.Find(id);
-            _context.Users.Remove(user);
-            _context.SaveChanges();
+        // [HttpDelete("{id:int}")]
+        // public ActionResult<User> Delete(int id)
+        // {
+        //     User user = _context.Users.Find(id);
+        //     _context.Users.Remove(user);
+        //     _context.SaveChanges();
 
-            return user;
-        }
+        //     return user;
+        // }
     }
 }
--- server/Services/UserService.cs
+++ server/Services/UserService.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using server.Models;
 namespace server.Services
 {
-    // public class UserService : DatabaseService
-    // {
+    public class UserService
+    {
+        public UserService(databaseContext context)
+        {
+            _context = context;
+        }
 
-    // }
+        private databaseContext _context;
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _context.Users.ToList();
+        }
+    }
 }
