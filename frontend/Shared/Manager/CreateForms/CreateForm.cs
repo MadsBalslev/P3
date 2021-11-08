b421faba9ba36a5eb21e883993acb33c2459071b
CasperStaahl <cstahl20@student.aau.dk>
2021-11-08 16:41:59 +0100

    Update CreateForm.cs

--- frontend/Shared/Manager/CreateForms/CreateForm.cs
+++ frontend/Shared/Manager/CreateForms/CreateForm.cs
@@ -3,22 +3,38 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
-
+using MudBlazor;
 
 public abstract class CreateForm<T> : ComponentBase where T : IManageable, new()
 {
+    [Inject]
+    private IHttpClientFactory _clientFactory { get; set; }
+
+    [Inject]
+    private IManagerService _managerService { get; set; }
+
     private readonly APIAttribute _apiAttribute =
     Attribute.GetCustomAttribute(typeof(T), typeof(APIAttribute)) as APIAttribute;
 
-    protected T _precreateItem = new T();
+    protected MudForm _form;
 
-    [Inject]
-    private IHttpClientFactory _clientFactory { get; set; }
+    protected T _precreateItem = new T();
     
     protected async Task OnAddItem()
     {
-        await PostItem();
-        StateHasChanged();
+        await _form.Validate();
+        if (_form.IsValid)
+        {
+            // BUG _form.Reset does not work, this seems to be a problem with MudBlazor
+            // _form.Reset();
+
+            await PostItem();
+            await _managerService.RequestRefresh();
+
+            // this is not a great solution to the problem above as it breaks the DatePickers :(
+            _precreateItem = new T();
+            _form.ResetValidation();
+        }
     }
 
     protected async Task PostItem()
@@ -35,13 +51,4 @@ public abstract class CreateForm<T> : ComponentBase where T : IManageable, new()
         {
         }
     }
-
-    // [Parameter]
-    // public EventCallback<T> OnAddPoster { get; set; }
-
-    // [Parameter]
-    // public EventCallback OnCancel { get; set; }
-
-
-
 }
