1f37369a05381f2f4cea7b8cad39ffbc63515604
CasperStaahl <cstahl20@student.aau.dk>
2021-11-09 13:27:10 +0100

    Update IManagerService.cs

--- frontend/Shared/Manager/IManagerService.cs
+++ frontend/Shared/Manager/IManagerService.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Threading.Tasks;
 
-public interface IManagerService
+public interface IManagerService<T>
 {
     event Func<Task> RefreshRequested;
     Task RequestRefresh();
+    public T SelectedItem { get; set; }
 }
