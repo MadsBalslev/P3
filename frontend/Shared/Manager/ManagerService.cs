5e2904e8fab16f562f01d2413f332903fd1aef79
CasperStaahl <cstahl20@student.aau.dk>
2021-11-09 13:27:18 +0100

    Update ManagerService.cs

--- frontend/Shared/Manager/ManagerService.cs
+++ frontend/Shared/Manager/ManagerService.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Threading.Tasks;
 
-public class ManagerService : IManagerService
+public class ManagerService<T> : IManagerService<T> where T : new()
 {
     public event Func<Task> RefreshRequested;
 
     public async Task RequestRefresh()
     {
-        Console.WriteLine("RequestRefresh was called");
         await RefreshRequested?.Invoke();
     }
+
+    public T SelectedItem { get; set; } = new();
 }
