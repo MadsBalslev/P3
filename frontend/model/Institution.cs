be38532465109da9b0c6cf907eda36f62a29a910
Casper Ståhl <cstahl20@student.aau.dk>
2021-11-16 15:30:30 +0100

    Update Institution.cs

--- frontend/model/Institution.cs
+++ frontend/model/Institution.cs
@@ -1,6 +1,9 @@
 [API("/Institutions")]
 public class Institution
 {
+    public int institutionId { get; set; } = -1;
+
     public int id { get; set; } = -1;
+
     public string name { get; set; } = "nil";
 }
