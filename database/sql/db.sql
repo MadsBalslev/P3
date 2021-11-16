8aa8169642b84f2e8aa416d8e09286fadb7427f2
Simon <simonahtx16@gmail.com>
2021-11-16 11:17:19 +0100

    Updated database-file to contain passwords for users.

--- database/sql/db.sql
+++ database/sql/db.sql
@@ -24,6 +24,7 @@ CREATE TABLE IF NOT EXISTS `database`.`users` (
   `first_name` VARCHAR(256) NOT NULL,
   `last_name` VARCHAR(256) NOT NULL,
   `email` VARCHAR(256) NOT NULL,
+  `password` VARCHAR(256) NOT NULL,
   `phone_number` INT NOT NULL,
   `institution` INT,
   `role` INT NOT NULL,
