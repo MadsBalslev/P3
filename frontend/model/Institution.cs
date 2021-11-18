3fccac3a2c8d0f704e6db07655fcdb80e6cdca2c
CasperStaahl <cstahl20@student.aau.dk>
2021-11-18 15:33:22 +0100

    Update Institution.cs

--- frontend/model/Institution.cs
+++ frontend/model/Institution.cs
@@ -1,6 +1,13 @@
-public class Institution
+public class Institution : IManageable
 {
+    public int? id { get; set; }
+
     public string name { get; set; }
 
-    public int? id { get; set; }
+    public User admin { get; set; }
+
+    public string ToJSON()
+    {
+        throw new System.NotImplementedException();
+    }
 }
