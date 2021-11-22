ce6ffb11911f68d227f710ec15c57619dfa61779
Casper Ståhl <cstahl20@student.aau.dk>
2021-11-22 10:07:48 +0100

    Update SlideGenerator.js

--- frontend/wwwroot/js/SlideGenerator.js
+++ frontend/wwwroot/js/SlideGenerator.js
@@ -1,6 +1,9 @@
+const DISPLAY_POSTER_MAX_CALL_COUNT = 100;
+const API_BASE_ADDRESS = "http://localhost:5000";
+
 function starPosterScreen() {
     initializePage();
-    getTimerValueThenGetPostersThenDisplayPosters();
+    getTimerValueThenGetPostersThenDisplayPosters(0);
 }
 
 function initializePage() {
@@ -20,35 +23,38 @@ function initializePage() {
     document.body.appendChild(screenDiv);
 }
 
-function getTimerValueThenGetPostersThenDisplayPosters() {
+function getTimerValueThenGetPostersThenDisplayPosters(displayPostersCallCount) {
     var req = new XMLHttpRequest();
     req.overrideMimeType("application/json");
-    req.open('GET', "http://localhost:5000/metadata/1", true);
+    req.open('GET', API_BASE_ADDRESS + "/metadata/1", true);
     req.onload = function () {
         var timerValue = JSON.parse(req.responseText).timerValue;
-        getPostersThenDisplayPosters(timerValue);
+        getPostersThenDisplayPosters(timerValue, displayPostersCallCount);
     };
     req.send();
 }
 
-function getPostersThenDisplayPosters(timerValue) {
+function getPostersThenDisplayPosters(timerValue, displayPostersCallCount) {
     var req = new XMLHttpRequest();
     req.overrideMimeType("application/json");
-    req.open('GET', "http://localhost:5000/Posters", true);
+    req.open('GET', API_BASE_ADDRESS + "/Posters", true);
     req.onload = function () {
         var posters = JSON.parse(req.responseText);
-        displayPosters(timerValue, posters, 0)
+        displayPosters(timerValue, posters, 0, displayPostersCallCount)
     };
     req.send();
 }
 
-function displayPosters(timerValue, posters, posterIndex) {
+function displayPosters(timerValue, posters, posterIndex, displayPostersCallCount) {
     const image = document.getElementById("imageId");
+    displayPostersCallCount++;
 
     if (posterIndex < posters.length) {
         image.src = posters[posterIndex].image;
         posterIndex++;
-        setTimeout(displayPosters, timerValue, timerValue, posters, posterIndex);
+        setTimeout(displayPosters, timerValue, timerValue, posters, posterIndex, displayPostersCallCount);
+    } else if (displayPostersCallCount < DISPLAY_POSTER_MAX_CALL_COUNT) {
+        getTimerValueThenGetPostersThenDisplayPosters(displayPostersCallCount);
     } else {
         location.reload()
     }
