a952fb5adef3374dbe4e4ba33ee8515be7ba8ffa
Casper Ståhl <cstahl20@student.aau.dk>
2021-11-23 15:19:32 +0100

    Update ManagerForm.cs

--- frontend/Shared/Manager/ManagerForm.cs
+++ frontend/Shared/Manager/ManagerForm.cs
@@ -1,69 +1,28 @@
 using Microsoft.AspNetCore.Components;
-using Microsoft.Extensions.Configuration;
 using MudBlazor;
-using System;
-using System.Diagnostics;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace frontend.Shared.Manager
 {
-    public abstract class ManagerForm<T> : ComponentBase where T : IManageable, new()
+    public abstract class ManagerForm<T> : ApiComponent where T : IManageable, new()
     {
+        [Inject]
+        public IManagerService ManagerService { get; set; }
+
         protected MudForm _form;
 
-        [Inject]
-        private ISnackbar Snackbar { get; set; }
-
-        [Inject]
-        private IHttpClientFactory ClientFactory { get; set; }
-
-        [Inject]
-        protected IManagerService ManagerService { get; set; }
-
-        [Inject]
-        protected IConfiguration Configuration { get; set; }
-
-        protected async Task<HttpResponseMessage> UpdateModelWithItem(HttpMethod method, T item, string path)
+        protected async Task OnRequestAction(HttpMethod method, string apiPath, T item, bool validate)
         {
-            Debug.Assert(method == HttpMethod.Post || method == HttpMethod.Put || method == HttpMethod.Delete);
+            T responseItem;
 
-            HttpRequestMessage request = new HttpRequestMessage(method, path);
-            string requestMessage = item.ToJSON();
-            request.Content = new StringContent(requestMessage, Encoding.UTF8, "application/Json");
+            if (validate)
+                responseItem = await RequestHandler<T, T>(method, apiPath, item, optionalForm: _form, requestSuccessPopup: "Action was successful");
+            else
+                responseItem = await RequestHandler<T, T>(method, apiPath, item, requestSuccessPopup: "Action was successful");
 
-            HttpClient client = ClientFactory.CreateClient();
-
-            HttpResponseMessage response = await client.SendAsync(request);
-
-            return response;
-        }
-
-        protected async Task OnConfirmChanges(HttpMethod method, T item, string path)
-        {
-            try
-            {
-                await _form.Validate();
-                if (_form.IsValid)
-                {
-                    HttpResponseMessage response = await UpdateModelWithItem(method, item, path);
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        Snackbar.Add("Action successful", Severity.Success);
-                        await RefreshManager();
-                    }
-                    else
-                    {
-                        Snackbar.Add($"Action failed!, status code: {response.StatusCode}", Severity.Error);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Snackbar.Add($"Something went wrong: {e.Message}", Severity.Error);
-            }
+            if (responseItem != null)
+                await RefreshManager();
         }
 
         protected async Task OnCancel()
@@ -71,7 +30,7 @@ namespace frontend.Shared.Manager
             await RefreshManager();
         }
 
-        private async Task RefreshManager()
+        protected async Task RefreshManager()
         {
             _form.Reset();
             _form.ResetValidation();
