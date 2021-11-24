f4737b9a40ad730f436e157abb5464d4c0033efa
Casper Ståhl <cstahl20@student.aau.dk>
2021-11-24 13:34:28 +0100

    Update IUser.cs

--- frontend/model/IUser.cs
+++ frontend/model/IUser.cs
@@ -6,8 +6,10 @@ public interface IUser
     string email { get; set; }
     string phoneNumber { get; set; }
     string password { get; set; }
+    string Authorization { get; set; }
     int? role { get; set; }
     string RoleAsString { get; }
+    AccessLevel AccessLevel { get; }
     string name { get; set; }
     Institution institution { get; set; }
 }
