60994d3093d6d341f82d5278f8dc5876bd76aa6b
Simon <simonahtx16@gmail.com>
2021-11-24 14:10:29 +0100

    cw removed

--- frontend/Shared/ApiComponent.razor.cs
+++ frontend/Shared/ApiComponent.razor.cs
@@ -92,7 +92,6 @@ namespace frontend.Shared
             if (body != null)
             {
                 string requestMessage = body.ToJSON();
-                Console.WriteLine(requestMessage);
                 request.Content = new StringContent(requestMessage, Encoding.UTF8, "application/Json");
             }
 
