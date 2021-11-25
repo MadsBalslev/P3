7e16d88d49c0bc1bad540358436b2d951488da28
Casper Ståhl <cstahl20@student.aau.dk>
2021-11-25 09:58:49 +0100

    Update CurrentUserService.cs

--- frontend/model/CurrentUserService.cs
+++ frontend/model/CurrentUserService.cs
@@ -2,4 +2,10 @@ class CurrentUserService : ICurrentUserService
 {
     public string Authorization { get; set; }
     public User User { get; set; } = new User(0, "", "", "", "", "", AccessLevel.User, new Institution());
+
+    public void Clear()
+    {
+        Authorization = null;
+        User = new User(0, "", "", "", "", "", AccessLevel.User, new Institution());
+    }
 }
