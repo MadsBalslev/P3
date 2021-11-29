f6e94eb1b3b36bb5499c9e22efe69dd429f7a517
Casper Ståhl <cstahl20@student.aau.dk>
2021-11-29 11:19:19 +0100

    Update CurrentUserService.cs

--- frontend/model/CurrentUserService.cs
+++ frontend/model/CurrentUserService.cs
@@ -2,7 +2,7 @@ using frontend.Shared;
 
 class CurrentUserService : ICurrentUserService
 {
-    public bool isLoggedIn { get => Authorization == null; }
+    public bool isLoggedIn { get => Authorization != null; }
     public string Authorization { get; set; }
 
     public User User { get; set; } = new User(0, "", "", "", "", "", AccessLevel.User, new Institution());
