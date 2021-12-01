108530d1efd8b8d89f812d804ca23e3cc4ca7a95
CasperStaahl <cstahl20@student.aau.dk>
2021-12-01 15:01:23 +0100

    Update PostersController.cs

--- server/Controllers/PostersController.cs
+++ server/Controllers/PostersController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using server.Services;
 
 namespace server.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class PostersController : ControllerBase
