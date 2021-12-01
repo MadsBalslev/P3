3d109ab07ce32e92638367bf7f057d52289d5ca2
CasperStaahl <cstahl20@student.aau.dk>
2021-12-01 15:16:09 +0100

    Update UnitTest1.cs

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,13 +1,9 @@
-using System.Configuration;
-using System.Linq;
+using System.IO;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using server;
-using server.Entities;
 using Xunit;
 
 public class IntegrationTests
@@ -43,6 +39,20 @@ public class GenericControllerTests : IntegrationTests
     }
 
     [Theory]
+    [InlineData("/MetaData/1")]
+    [InlineData("/Zones/Active/1")]
+    public async Task Get_EndpointsReturnOkNoAuthorizationHeader(string url)
+    {
+        // Arrange
+
+        // Act
+        var response = await _client.GetAsync(url);
+
+        // Assert
+        Assert.Equal("OK", response.StatusCode.ToString());
+    }
+
+    [Theory]
     [InlineData("/Institutions")]
     [InlineData("/Posters")]
     [InlineData("/Schedule")]
@@ -64,7 +74,21 @@ public class GenericControllerTests : IntegrationTests
 
 public class PostersControllerTests : IntegrationTests
 {
+    [Fact]
+    public async Task Post()
+    {
+        // Arrange
+        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "/Posters");
+        request.Headers.Add("Authorization", $"Basic YWRtaW46JDJhJDExJGdYVFdVbWhqYmhiak9xelQ2QW5mVk9SZlBqVlhUL3c0VVFoUFhrcjNHNnZzVnM3eFEzYS9D");
 
+        // Act
+        HttpResponseMessage response = await _client.SendAsync(request);
+
+        using Stream responseStream = await response.Content.ReadAsStreamAsync();
+        string body = await JsonSerializer.DeserializeAsync<string>(responseStream);
+
+        // Assert
+        Assert.Equal("OK", );
+    }
 
 }
-
