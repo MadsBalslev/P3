96b2ebdd2ed941354edb83960604f834597b4c4d
CasperStaahl <cstahl20@student.aau.dk>
2021-12-03 13:54:17 +0100

    Update UsersControllerTests.cs

--- tests/Integrationtests/UsersControllerTests.cs
+++ tests/Integrationtests/UsersControllerTests.cs
@@ -10,17 +10,6 @@ namespace tests.Integrationtests
     // [Collection("API tests")]
     public class UsersControllerTests : IntegrationTests
     {
-        // https://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings
-        private static Random random = new Random();
-
-        // https://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings
-        private static string RandomString(int length)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, length)
-                .Select(s => s[random.Next(s.Length)]).ToArray());
-        }
-
         [Fact]
         public async Task Post()
         {
